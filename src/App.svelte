9e61aeaee8b96d409dc086dd9d2d3cd578ddcb08
Michal Bocek <michal.bocek@embedit.cz>
2021-11-03 19:40:03 +0100

    feat(svelte): initial svelte setup

---
+++ src/App.svelte
@@ -0,0 +1,31 @@
+<script>
+    import { Styles, Navbar, NavbarBrand, Nav, NavItem, NavLink, Container, Row, ListGroup, ListGroupItem } from "sveltestrap";
+    import Computers from "./computer/Computers.svelte";
+</script>
+
+<Styles />
+
+<Navbar class="navbar-dark bg-dark p-0">
+    <NavbarBrand href="/">Evidence</NavbarBrand>
+    <Nav navbar>XXX</Nav>
+</Navbar>
+
+<Container fluid>
+    <Row>
+        <Navbar class="col-md-2 d-md-block bg-light sidebar">
+            <Nav>
+                <NavItem>
+                    <NavLink active>Computers</NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink>Test</NavLink>
+                </NavItem>
+            </Nav>
+        </Navbar>
+        <main role="main" class="col-md-10">
+            <Container>
+                <Computers />
+            </Container>
+        </main>
+    </Row>
+</Container>
---
+++ src/computer/Computers.svelte
@@ -0,0 +1,33 @@
+<script>
+    import { Container, Table } from "sveltestrap";
+
+	import { onMount } from "svelte";
+	import { httpGet } from "../common/api.js";
+    
+	let computers = [];
+	onMount(async function() {
+		const { data } = await httpGet("/");
+		computers = data;
+	});
+</script>
+
+<Table>
+    <thead>
+        <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>Domain</th>
+            <th>Vendor</th>
+        </tr>
+    </thead>
+    <tbody>
+        {#each computers as computer}
+        <tr>
+            <th scope="row">{computer.id}</th>
+            <td>{computer.name}</td>
+            <td>{computer.domain}</td>
+            <td>{computer.vendor}</td>
+        </tr>
+        {/each}
+    </tbody>
+</Table>
