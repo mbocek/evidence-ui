415f1b9d445e6c788d556dd4263365dce73f25a0
Michal Bocek <michal.bocek@embedit.cz>
2021-12-22 14:04:19 +0100

    feat(error): improved error handling

--- src/component/ErrorMessage.svelte
+++ src/component/ErrorMessage.svelte
@@ -1,16 +1,33 @@
 <script>
-    import { Alert } from "sveltestrap";
+    import Fa from 'svelte-fa'
+    import { faInfoCircle } from '@fortawesome/free-solid-svg-icons'
+    import { Alert, Collapse } from "sveltestrap";
 
     export let error;
+    let isOpen;
 </script>
 
 {#if error}
 <Alert color="danger" dismissible>
-    {#each error.errors as message}
-    <h4 class="alert-heading text-capitalize">{message}</h4>
-    {/each}
+    <div class="d-flex justify-content-between">
+        <div>
+            <h4 class="alert-heading">{error.message}</h4>
+        </div>
+        <div>
+            <a href="#" class="btn btn-secondary btn-sm" on:click={() => (isOpen = !isOpen)}><Fa icon={faInfoCircle}/></a>
+        </div>
+    </div>
     <hr/>
+    {#if error.message === "Validation error"}
+    {#each error.fieldErrors as fieldError}
+        {fieldError.object}.{fieldError.field} - {fieldError.message}<br/>
+    {/each}
+    {:else}
     <h5>Status code: {error.status}</h5>
-    {error.message}
+    {/if}
+    <Collapse {isOpen}>
+        <small>{error.timestamp}</small>
+        <pre style="white-space: normal;">{error.detail}</pre>
+    </Collapse>
 </Alert>
 {/if}
